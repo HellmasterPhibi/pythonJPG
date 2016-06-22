a55927a14eb625cd0ec530cbc2a5576956f070ab
HellmasterPhibi <hellmasterphibi@gmail.com>
2016-06-23 01:01:00 +0200

    added both secret message and an image file in the exif

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def getExif(i):
 
 img = Image.open('skluzavka.jpg')
 exifDict = piexif.load(img.info["exif"])
-print(img.format, img.size, img.mode)
 width, height = img.size
 box = (0, 0, width/2, height)
 region = img.crop(box)
@@ -28,23 +27,36 @@ region.save('vyrez.jpg','jpeg', exif=origExif)
 
 vyrez = Image.open('vyrez.jpg')
 
-print(getExif(img))
-print(getExif(vyrez))
+#print(getExif(img))
+#print(getExif(vyrez))
 
 
-exifDict = piexif.load(img.info["exif"])
-
+exifDict = piexif.load(vyrez.info["exif"])
+print(exifDict)
 
 exifDict["0th"][piexif.ImageIFD.XResolution] = (width, 1)
 exifDict["0th"][piexif.ImageIFD.YResolution] = (height, 1)
 exifDict["1st"][piexif.ImageIFD.Software] = "-------------Tajna zprava---------------------"
 
+#image for meta exif loading
+o = io.BytesIO()
+thumb_im = Image.open("wolf.jpg")
+thumb_im.thumbnail((50, 50), Image.ANTIALIAS)
+thumb_im.save(o, "jpeg")
+thumbnail = o.getvalue()
+
+exifDict["thumbnail"] = thumbnail
 exifBytes = piexif.dump(exifDict)
 #extraBytes = piexif.dump(extra)
-img.save('newExif.jpg', "jpeg")
+vyrez.save('newExif.jpg', "jpeg")
 #print(getExif(img2))
 piexif.insert(exifBytes, "newExif.jpg")
 img2 = Image.open('newExif.jpg')
 
 exifDict2 = piexif.load(img2.info["exif"])
 print(exifDict2)
+
+img2.show()
+
+#print text message stored in exif
+print(exifDict2["1st"][piexif.ImageIFD.Software])
