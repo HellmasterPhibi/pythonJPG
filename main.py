5ae9884b5bdcc122296cc44e6b3d36f073e9de98
HellmasterPhibi <hellmasterphibi@gmail.com>
2016-06-22 15:45:52 +0200

    seeing how crop works

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from __future__ import print_function
 from PIL import Image
+
+
 img = Image.open('skluzavka.jpg')
-img.show()
+
+print(img.format, img.size, img.mode)
+box = (0, 0, 400, 400)
+region = img.crop(box)
+region.show()
