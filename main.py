e4e6488301bff66eccb90d3b001a648bed9dd997
HellmasterPhibi <hellmasterphibi@gmail.com>
2016-06-22 15:55:48 +0200

    half of the image has been cropped and saved

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIL import Image
 img = Image.open('skluzavka.jpg')
 
 print(img.format, img.size, img.mode)
-box = (0, 0, 400, 400)
+box = (0, 0, 1024, 1536)
 region = img.crop(box)
 region.show()
+region.save('vyrez.jpg','jpeg')
